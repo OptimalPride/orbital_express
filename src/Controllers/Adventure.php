5a243f4ab4e05037aeb825bdee6ec6bd8feee63d
OptimalPride <theoptimalpride@gmail.com>
2017-05-29 11:33:14 +0200

    Form better

--- src/Controllers/Adventure.php
+++ src/Controllers/Adventure.php
@@ -11,6 +11,11 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class Adventure
 {
@@ -50,19 +55,43 @@ class Adventure
 	        "description" => "Description de l'aventure",
 	        "pitch" => "Pitch de l'aventure, l'accroche"
 	    );
-	    $form = $app['form.factory']->createBuilder(FormType::class, $data)
-	        ->add('name')
-	        ->add('description')
-	        ->add('pitch')
+	    $adventureform = $app['form.factory']->createBuilder(FormType::class, $data)
+			->add("name", TextType::class, array(
+				"constraints" => array(
+					new Assert\NotBlank(),
+					new Assert\Length(array(
+						"min" => 3,
+						"max" => 20
+					))
+				)
+			))
+			-> add('description', TextareaType::class, Array(
+				"constraints" => array(
+					new Assert\NotBlank(),
+					new Assert\Length(array(
+						"min" => 5,
+						"max" => 200
+					)
+				))
+			))
+			-> add('pitch', TextareaType::class, Array(
+				"constraints" => array(
+					new Assert\NotBlank(),
+					new Assert\Length(array(
+						"min" => 3,
+						"max" => 2000
+					)
+				))
+			))
 	        ->add('submit', SubmitType::class, [
 	            'label' => 'Ajouter aventure',
 	        ])
 	        ->getForm();
 
-	    $form->handleRequest($request);
+	    $adventureform->handleRequest($request);
 
-	    if ($form->isValid()) {
-	        $data = $form->getData();
+	    if ($adventureform->isValid()) {
+	        $data = $adventureform->getData();
 	        if (isset($data["name"]) && isset($data["description"]) && isset($data["pitch"])) {
 	            $information = array("name" => $data["name"], "description" => $data["description"], "pitch" => $data["pitch"]);
 	            $msg = $app["dao.adventure"]->addNewAdventure($information);
@@ -73,7 +102,7 @@ class Adventure
 	            throw new \Exception("Veuillez remplir toutes les informations");
 	        }
 	    }
-	    return $app['twig']->render('backoffice/createadventure.html.twig', array('form' => $form->createView()));
+	    return $app['twig']->render('backoffice/createadventure.html.twig', array('adventureform' => $adventureform->createView()));
 	}
 
 	public function modifyAdventure(Application $app, Request $request, $id_adventure){
