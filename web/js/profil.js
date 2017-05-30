471a359b727c7da8c17d0eae472898301cb5c5bc
badresassi <badresassi@gmail.com>
2017-05-30 16:15:12 +0200

    ...

--- web/js/profil.js
+++ web/js/profil.js
@@ -1,32 +1,34 @@
 $(function(){
 	$("a.nouvelle-aventure").click(function(e){
-		var getAdventures = $.ajax({ 	
+		var getAdventures = $.ajax({
 			url: baseUrl+"getavailableadventures/",
 			method: "POST",
-		}); 
+		});
 		getAdventures.done(function(msg) {
 			$("div.contenu_nouvelle_aventure").html(msg);
-		});	
-	})
+		});
+	});
 
 	$("a.continuer-aventure").click(function(e){
-		var continueAdventure = $.ajax({ 	
+		$("div.contenu_nouvelle_aventure").empty();
+		var continueAdventure = $.ajax({
 			url: baseUrl+"continue/",
 			method: "POST",
-		}); 
+		});
 		continueAdventure.done(function(msg) {
 			$("div.contenu_continuer_aventure").html(msg);
-		});	
-	})
+		});
+	});
 
 	$("a.sauvegardes").click(function(e){
-		var gestionSauvegardes = $.ajax({ 	
+		$("div.contenu_nouvelle_aventure").empty();
+		var gestionSauvegardes = $.ajax({
 			url: baseUrl+"gestionusersaves/",
 			method: "POST",
-		}); 
+		});
 		gestionSauvegardes.done(function(msg) {
 			console.log(msg);
 			$("div.contenu_sauvegardes").html(msg);
-		});	
-	})
-})
+		});
+	});
+});
